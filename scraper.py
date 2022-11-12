886a0ce6c636e1e97eeaf3aa04defc084933fc95
Peekoe <peekoecode@gmail.com>
2022-11-12 02:48:17 -0800

    repo config + types jsons

--- scraper.py
+++ scraper.py
@@ -1,31 +1,36 @@
+from asyncio import sleep
 import requests
 from bs4 import BeautifulSoup
 import re
 
+# https://stackoverflow.com/questions/2319019/using-regex-to-remove-comments-from-source-files
+# try this too: https://codereview.stackexchange.com/questions/148305/remove-comments-from-c-like-source-code
 def remove_comments(string):
-    pattern = r'//.*?\n'
-    return re.sub(pattern, '', string)
+  pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+  regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+  def _replacer(match):
+    if match.group(2) is not None:
+      return ""
+    else:
+      return match.group(1)
+
+  return regex.sub(_replacer, string)
 
 
+with open("endpoints.txt", "r") as f:
+  endpoints = f.readlines()
 
+for end in endpoints:
+  page = requests.get(end.strip())
+  soup = BeautifulSoup(page.content, 'html.parser')
+  tags = soup.find_all('div', class_='object_definition')
 
-# read html file
-with open('example.htm', 'r') as html_file:
-  content = html_file.read()
-  # parse html
-  soup = BeautifulSoup(content, 'html.parser')
-
-  # find tags
-  tags = soup.find_all(
-    'pre', class_='example code prettyprint prettyprinted')
-
-  # filter strings that start with {
-  # and end with }
-  # and remove comments
   for tag in tags:
-    code = tag.text
-    if code.startswith('{') and code.endswith('}'):
-      code = remove_comments(code)
-      # write code to file
-      with open('example.json', 'w') as json_file:
-        json_file.write(code)
+    name = tag.find('a')['name']
+    content = remove_comments(
+      tag.find('pre', class_='example code prettyprint').string)
+    with open(f"types/json/{name}.json", "w") as f:
+      f.write(content)
+
+  sleep(0.1)
---
+++ types/json/!README.md
@@ -0,0 +1,11 @@
+# JSON Types
+These example JSON values are scraped from the Canvas API docs, and then
+turned into TypeScript interfaces using [QuickType](https://quicktype.io/).
+
+To scrape and generate updated types, run these commands:
+
+```
+python3 scraper.py
+cd types
+npx quicktype json -o models.ts
+```
