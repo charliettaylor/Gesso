87805d08683056074deba5708f773ef426a3e68c
Peekoe <peekoecode@gmail.com>
2022-11-23 23:34:05 -0800

    Model name fix

--- src/Progress.ts
+++ src/Progress.ts
@@ -1,13 +1,13 @@
 import { BaseApi } from "./BaseApi.ts";
 import { Configuration } from "./Configuration.ts";
-import { Progress } from "../types/models.ts";
+import { Progress as ProgressModel } from "../types/models.ts";
 
 export class Progress extends BaseApi {
   constructor(config: Configuration) {
     super(config);
   }
 
-  public async queryProgress(id: string, body?: unknown): Promise<Progress> {
+  public async queryProgress(id: string, body?: unknown): Promise<ProgressModel> {
     const endpoint = `/api/v1/progress/${id}`;
     const url = new URL(endpoint, this.configuration.domain);
 
@@ -19,7 +19,7 @@ export class Progress extends BaseApi {
     return Promise.reject(response);
   }
 
-  public async cancelProgress(id: string, body?: unknown): Promise<Progress> {
+  public async cancelProgress(id: string, body?: unknown): Promise<ProgressModel> {
     const endpoint = `/api/v1/progress/${id}/cancel`;
     const url = new URL(endpoint, this.configuration.domain);
 
@@ -35,7 +35,7 @@ export class Progress extends BaseApi {
     course_id: string,
     id: string,
     body?: unknown,
-  ): Promise<Progress> {
+  ): Promise<ProgressModel> {
     const endpoint = `/api/v1/api/lti/courses/${course_id}/progress/${id}`;
     const url = new URL(endpoint, this.configuration.domain);
 
